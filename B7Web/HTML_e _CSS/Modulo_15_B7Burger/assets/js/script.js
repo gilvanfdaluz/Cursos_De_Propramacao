80cbda9ce2af7f31ea9c001dfbe81ff09f90e379
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2024-10-17 02:46:52 -0300

    Atualizado

--- B7Web/HTML_e _CSS/Modulo_15_B7Burger/assets/js/script.js
+++ B7Web/HTML_e _CSS/Modulo_15_B7Burger/assets/js/script.js
@@ -2,6 +2,5 @@ const menuIcon = document.querySelector('.menu');
 const nav = document.querySelector('nav');
 
 menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('open');
     nav.classList.toggle('open');
 });
