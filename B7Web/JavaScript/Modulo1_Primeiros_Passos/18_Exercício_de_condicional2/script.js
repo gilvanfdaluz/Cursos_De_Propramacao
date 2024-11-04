5d7b9222435378d2a1636155f3a69eed8ccc8af4
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2024-11-04 20:37:51 -0300

    Atualizado

---
+++ B7Web/JavaScript/Modulo1_Primeiros_Passos/18_Exercício_de_condicional2/script.js
@@ -0,0 +1,26 @@
+//Substitua o @ pela condicional correta para que
+//o resultado seja TUE
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+//Substitua o @ pela conficional correta para que
+// o resultado seja FALSE
+let w = '10';
+let z = 10;
+console.log(w === z);
+
+//substitua o @ pela condiconal correta para que
+//o resultado seja TRUE
+let a = 10;
+let b = 5;
+console.log(x != y);
+
+//Crie uma condicional para verificar se o preço da
+//carne está barato ou caro0. PS: Até 45 está barato
+let preco = 45.3
+if(preco <= 45){
+    console.log('Está barato');
+} else{
+    console.log('Está caro');
+}
