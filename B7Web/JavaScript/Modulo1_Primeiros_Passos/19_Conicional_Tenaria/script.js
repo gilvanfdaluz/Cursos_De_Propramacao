457f4ff31ddf0916a9bfe080f22813f77832044f
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2024-11-04 21:29:36 -0300

    Atualização

---
+++ B7Web/JavaScript/Modulo1_Primeiros_Passos/19_Conicional_Tenaria/script.js
@@ -0,0 +1,9 @@
+let isMember = true;
+let shipping = isMember ? 2 : 10;
+console.log('Frete: ' + shipping);
+
+//============================
+
+let age = 19;
+let isAdult = ((age >= 18 && age < 60) ? 'Sim' : 'Não');
+console.log(isAdult);
