ae5ddb0b4b9317df2ceee072b6fa6c4ab712ad0c
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2024-11-08 23:00:36 -0300

    Atualizado

---
+++ B7Web/JavaScript/Modulo1_Primeiros_Passos/19_Switch/script.js
@@ -0,0 +1,17 @@
+//Policial
+//Bombeiro
+//Fiscal
+
+let profession = 'fiscal';
+
+switch (profession) {
+    case 'fiscal':
+        console.log('Sua camisa é verde');
+        break;
+    case 'bombeiro':
+    console.log('Sua camisa será vermelha');
+        break;
+    case 'policial':
+        console.log('sua camisa será azul');
+        break
+}
