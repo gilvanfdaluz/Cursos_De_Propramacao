184ad23361af9c89fb65fdf4f2a07ec3af5e819f
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2024-12-05 23:34:45 -0300

    Atualizado

---
+++ B7Web/JavaScript/Modulo2_JavaScript_Basico/13_Introdução_a_Array copy/script.js
@@ -0,0 +1,15 @@
+let ingrediente = [
+    'ovos',
+    'manteiga',
+    'farinha',
+    'açucar'
+    ];
+
+    //ingrediente.push('coco'); //Adiciona itens
+    //ingrediente.shift() //Remove oprimeiros;
+   // ingrediente.pop() //Remove o ultimo;
+    
+    console.log(ingrediente);
+ 
+
+console.log(`Total de ingredientes ${ingrediente.length}`);
--- B7Web/JavaScript/Modulo2_JavaScript_Basico/13_Introdução_a_Array/script.js
+++ B7Web/JavaScript/Modulo2_JavaScript_Basico/13_Introdução_a_Array/script.js
@@ -9,5 +9,5 @@ console.log(lista[1][0]);
 
 // pegando a posição do array com variavel
 let lista2 = ['xy', ['xx', 'yy']];
-let itens = lista[1];
+let itens = lista2[1];
 console.log(itens[0]);
