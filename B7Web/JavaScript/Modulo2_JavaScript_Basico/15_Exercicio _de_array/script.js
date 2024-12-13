46c5e3899b26264b9f83c8aa9ff1db84f690e1db
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2024-12-12 22:16:39 -0300

    Atualizado

---
+++ B7Web/JavaScript/Modulo2_JavaScript_Basico/15_Exercicio _de_array/script.js
@@ -0,0 +1,18 @@
+//1. No array babaixo, qual número que pega a Ferrari?
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let x = 1;
+console.log('1. + carros ' + [x]);
+
+//2. troque a Ferrari por um  audi
+carros[1] = 'Audi'
+console.log('.2 Lista com Aldi');
+console.log(carros);
+
+//3. Adicione um Volvo na lista
+carros.push('Volvo')
+console.log('Lista com volvo');
+console.log(carros)
+
+//4. Exiba quantos itens tem no array
+console.log('4. Itens no array');
+console.log(carros.length);
