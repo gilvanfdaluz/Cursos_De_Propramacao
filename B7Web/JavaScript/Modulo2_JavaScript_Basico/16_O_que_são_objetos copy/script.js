4aaa48861917ea5bb9469a46cba7872080ca0a14
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2025-01-04 00:19:48 -0300

    Atualizado

---
+++ B7Web/JavaScript/Modulo2_JavaScript_Basico/16_O_que_são_objetos copy/script.js
@@ -0,0 +1,27 @@
+let veiculo = {
+    nome: 'Celta',
+    ano: 2024,
+    pais: 'Brasil',
+    cambio: ['Autmomático', 'Manual'],
+    caracteristicas: {
+        cor: ['Branco', 'Azul'],
+        potencia: '150-CV'
+    }
+}
+veiculo.nome = 'Golf';
+veiculo.ano += 5;
+veiculo.caracteristicas.cor.push('Verde');
+console.log(`Veiculo ${veiculo.nome}, ano ${veiculo.ano}, nas cores ${veiculo.caracteristicas.cor[2]}`);
+
+//================================================
+
+let personagem = {
+    nome: 'Paulo',
+    idade: 65,
+    carros: [
+        {modelo: 'Fiat', cor: ' Preto'},
+        {modelo: 'Ferrari', cor: ' Vermelho'}
+    ]
+}
+
+console.log(personagem.carros[0].modelo + personagem.carros[0].cor);
