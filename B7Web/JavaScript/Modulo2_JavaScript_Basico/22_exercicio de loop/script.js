7555d687b42b34165708cc087ee179684269954b
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2025-02-11 21:30:15 -0300

    Atualizado

---
+++ B7Web/JavaScript/Modulo2_JavaScript_Basico/22_exercicio de loop/script.js
@@ -0,0 +1,16 @@
+//Faça um loop que mostre todas as frutas
+
+let frutas = ['uva', 'maçã', 'banana'];
+for (let n = 0; n < frutas.length; n++) {
+    console.log(frutas[n]);
+}
+
+
+
+// Conte de 1 até 100 através de um WEILE
+
+let i = 0;
+while (i < 10) {
+    console.log(`O númeto da vez é ${i}`)
+    i++
+}
