14eac4379f670c8b494d32e3374456d7d63b3a65
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2025-03-20 21:55:47 -0300

    Atualizado

---
+++ B7Web/JavaScript/Modulo3_Eventos_e_manipulação_de_Dom/02_Selecionando_Elementos/script.js
@@ -0,0 +1,14 @@
+let elementos = document.getElementsByTagName('h1');
+console.log(elementos[0]);
+console.log(elementos[1]);
+
+
+/*
+DICAS :
+
+document.addEventListener("DOMContentLoaded", function () {
+    let elementos = document.getElementsByTagName('h1');
+    console.log(elementos[0]); // Primeiro <h1>
+    console.log(elementos[1]); // Segundo <h1> (caso exista)
+});
+*/
