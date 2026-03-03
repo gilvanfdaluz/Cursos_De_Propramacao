659fdfc984b2cea6ecab9b8836331c36f94f591b
Gilvan Ferreira da Luz <125775442+gilvanfdaluz@users.noreply.github.com>
2026-03-03 18:42:01 -0300

    Atualizado

---
+++ B7Web/JavaScript/Modulo3_Eventos_e_manipulação_de_Dom/03_Selecionando_Elementos /script.js
@@ -0,0 +1,14 @@
+/*document.getElementsByTagName("div") = Selecina pelo name da tag
+document.getElementById("lista") = Seleciona pelo nome do Id
+document.getElementsByTagName("div") = Selecina pelo name da tag
+
+Sempre que tiver Elements no plural ou SelectorAll retorna uma array e o elemento dentro do array
+
+Seleção de forma simples:
+document.querySelector("#lista") = Id
+document.querySelector("lbtn") = Class
+document.querySelector("h1") = Tag Name
+
+Encontrar vários elementos:
+document.querySelectorAll("li")
+document.querySelectorAll("#lista ul li")*/
