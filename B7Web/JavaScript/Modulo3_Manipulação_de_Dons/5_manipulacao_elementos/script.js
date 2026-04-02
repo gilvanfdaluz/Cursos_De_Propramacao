c269ef1cb6d53bbf13cd8e5b80b2c106038ca624
Gilvan Ferreira <gilvanferreiralz@hotmail.com>
2026-04-02 20:49:59 -0300

    Atualizado

--- B7Web/JavaScript/Modulo3_Manipulação_de_Dons/5_manipulacao_elementos/script.js
+++ B7Web/JavaScript/Modulo3_Manipulação_de_Dons/5_manipulacao_elementos/script.js
@@ -1,4 +1,11 @@
-function clicou() {
+/*function clicou() {
     const teste = document.querySelector("#teste");
     console.log(teste.children);
+}*/
+
+function clicou() {
+    const teste = document.querySelector("#teste");
+    const ul = teste.querySelector("ul");
+
+    ul.children[0].innerHTML = "item alterdo";
 }
